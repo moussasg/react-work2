bc48e2adeadfb019f8cf69e331ee0172a8b49735
moussasg <117439847+moussasg@users.noreply.github.com>
2023-01-12 10:09:04 +0100

    react2

--- src/Component/Profile/ProfilePhoto.js
+++ src/Component/Profile/ProfilePhoto.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import './style.css'
-import Img from './monimg.JPG'
+import Img from '../../assets/monimg.JPG'
 export default function ProfilePhoto() {
   return (
     <>
     <div className="gr">
-    <div className='cv'> cv </div>
+    <div className="cv">
+    cv 
+    <div >
+      <h1> Skills</h1>
+    </div>
+    
+    </div>
     <img className="ph" src={Img}></img>
-    <div className="bord">  </div>
+    <div className="bord">  
+
+    </div>
+
     </div>
     </>
   )
